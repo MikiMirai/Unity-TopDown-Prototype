5d1768089bc96270230cf060fb44d1f65fe01ea9
MikiMirai <100376192+MikiMirai@users.noreply.github.com>
2025-08-30 21:24:56 +0300

    Added a manager script to handle game over and buttons

---
+++ Assets/Scripts/StageManager.cs
@@ -0,0 +1,36 @@
+using System.Windows;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StageManager : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private GameObject gameOverPanel;
+
+    private void Awake()
+    {
+        EventManager.OnPlayerDeath += OnGameOver;
+    }
+
+    private void OnDestroy()
+    {
+        EventManager.OnPlayerDeath -= OnGameOver;
+    }
+
+    void OnGameOver()
+    {
+        gameOverPanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void RestartScene()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadSceneAsync(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
