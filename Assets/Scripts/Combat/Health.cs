1cfc283b8904b3ccb2079e1082f96a39534139f0
MikiMirai <100376192+MikiMirai@users.noreply.github.com>
2025-08-30 20:47:35 +0300

    Modified the Health script to use the new effect handler

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -2,25 +2,45 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [Header("References")]
+    [SerializeField] private EffectHandler effectHandler;
+
+    [SerializeField] private bool isEnemyObject = false;
     [SerializeField] private int maxHealth = 5;
     public int CurrentHealth;
 
     void Awake()
     {
         CurrentHealth = maxHealth;
+
+        if (effectHandler != null)
+        {
+            TryGetComponent<EffectHandler>(out EffectHandler handlerRef);
+            effectHandler = handlerRef;
+        }
     }
 
-    // Called by the bullet (or any other damage source)
     public void TakeDamage(int amount)
     {
         CurrentHealth -= amount;
         if (CurrentHealth <= 0)
+        {
             Die();
+        }
+        else
+        {
+            effectHandler?.StartHitFlash();
+        }
     }
 
     private void Die()
     {
         Debug.Log($"{gameObject.name} died");
+
+        if(isEnemyObject)
+        {
+            Destroy(gameObject, 1f);
+        }
         // TODO: Add death logic here (animation, respawn, etc.)
     }
 }
