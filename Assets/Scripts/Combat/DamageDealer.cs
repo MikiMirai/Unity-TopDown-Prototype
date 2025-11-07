6027f18ebb1a0ea7d66a8f8b64af33137bb45771
MikiMirai <100376192+MikiMirai@users.noreply.github.com>
2025-11-07 03:33:27 +0200

    Added damage dealder to collect the enemies hit

---
+++ Assets/Scripts/Combat/DamageDealer.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageDealer : MonoBehaviour
+{
+    bool canDealDamage;
+    List<GameObject> hasDealthDamage;
+
+    [SerializeField] private float weaponLength;
+    [SerializeField] private float weaponDamage;
+    [SerializeField] private LayerMask weaponMask;
+
+    private void Start()
+    {
+        canDealDamage = false;
+        hasDealthDamage = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, weaponLength, weaponMask))
+        {
+            if (!hasDealthDamage.Contains(hit.transform.gameObject))
+            {
+                Debug.Log("Dealt Damage!");
+                hasDealthDamage.Add(hit.transform.gameObject);
+            }
+        }
+    }
+
+    public void StartDealDamage()
+    {
+        Debug.Log("--- Damage Enabled");
+        canDealDamage = true;
+        hasDealthDamage.Clear();
+    }
+
+    public void EndDealDamage()
+    {
+        Debug.Log("--- Damage Disabled");
+        canDealDamage = false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(transform.position, transform.position + transform.forward * weaponLength);
+    }
+}
