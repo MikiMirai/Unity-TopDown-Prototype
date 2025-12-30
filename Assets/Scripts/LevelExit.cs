3a782325dfc820a4565222c2490eb2fda041c4c3
MikiMirai <100376192+MikiMirai@users.noreply.github.com>
2025-12-30 17:34:57 +0200

    Level progression

--- Assets/Scripts/LevelExit.cs
+++ Assets/Scripts/LevelExit.cs
@@ -2,13 +2,26 @@ using UnityEngine;
 
 public class LevelExit : MonoBehaviour
 {
+    [Header("Level Loading")]
+    [SerializeField] private bool loadNextLevel = false;
+    [SerializeField] private LevelLoader levelLoader;
+
+    [Header("UI")]
     [SerializeField] private GameObject winPanel;
     [SerializeField] private GameObject HUD;
 
+
     private void OnTriggerEnter(Collider other)
     {
-        winPanel.SetActive(true);
-        HUD.SetActive(false);
-        Time.timeScale = 0f;
+        if (loadNextLevel && levelLoader != null)
+        {
+            levelLoader.LoadNextLevel();
+        }
+        else
+        {
+            winPanel.SetActive(true);
+            HUD.SetActive(false);
+            Time.timeScale = 0f;
+        }
     }
 }
